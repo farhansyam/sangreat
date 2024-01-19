bb45dd2925e9a2847fdb466f0bda7499706868b2
muj4h1d1n <157119909+muj4h1d1n@users.noreply.github.com>
2024-01-19 19:47:16 +0700

    fix-form

--- packages/Webkul/Shop/src/Resources/views/checkout/onepage/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/checkout/onepage/index.blade.php
@@ -1,50 +1,83 @@
-{{-- SEO Meta Content --}}
+<!-- SEO Meta Content -->
 @push('meta')
-<meta name="description" content="@lang('shop::app.checkout.onepage.index.checkout')" />
+    <meta name="description" content="@lang('shop::app.checkout.onepage.index.checkout')"/>
 
-<meta name="keywords" content="@lang('shop::app.checkout.onepage.index.checkout')" />
+    <meta name="keywords" content="@lang('shop::app.checkout.onepage.index.checkout')"/>
 @endPush
 
-<x-shop::layouts :has-header="false" :has-feature="false" :has-footer="false">
-    {{-- Page Title --}}
+<x-shop::layouts
+    :has-header="false"
+    :has-feature="false"
+    :has-footer="false"
+>
+    <!-- Page Title -->
     <x-slot:title>
         @lang('shop::app.checkout.onepage.index.checkout')
-        </x-slot>
+    </x-slot>
 
-        {{-- Page Header --}}
-        <div class="lex flex-wrap">
-            <div class="w-full flex justify-between px-[60px] py-[17px] border border-t-0 border-b-[1px] border-l-0 border-r-0 max-lg:px-[30px] max-sm:px-[15px]">
-                <div class="flex items-center gap-x-[54px] max-[1180px]:gap-x-[35px]">
-                    <a href="{{ route('shop.home.index') }}" class="flex min-h-[30px]" aria-label="Bagisto ">
-                        <img src="{{ bagisto_asset('images/logo.svg') }}" alt="Bagisto " width="131" height="29">
-                    </a>
-                </div>
+    {!! view_render_event('bagisto.shop.checkout.onepage.header.before') !!}
+
+    <!-- Page Header -->
+    <div class="lex flex-wrap">
+        <div class="w-full flex justify-between px-[60px] py-4 border border-t-0 border-b border-l-0 border-r-0 max-lg:px-8 max-sm:px-4">
+            <div class="flex items-center gap-x-14 max-[1180px]:gap-x-9">
+                <a
+                    href="{{ route('shop.home.index') }}"
+                    class="flex min-h-[30px]"
+                    aria-label="@lang('shop::checkout.onepage.index.bagisto')"
+                >
+                    <img
+                        src="{{ core()->getCurrentChannel()->logo_url ?? bagisto_asset('images/logo.svg') }}"
+                        alt="{{ config('app.name') }}"
+                        width="131"
+                        height="29"
+                    >
+                </a>
             </div>
         </div>
+    </div>
 
-        <div class="container px-[60px] max-lg:px-[30px] max-sm:px-[15px]">
-            {{-- Breadcrumbs --}}
-            <x-shop::breadcrumbs name="checkout"></x-shop::breadcrumbs>
+    {!! view_render_event('bagisto.shop.checkout.onepage.header.after') !!}
 
-            <v-checkout>
-                {{-- Shimmer Effect --}}
-                <x-shop::shimmer.checkout.onepage />
-            </v-checkout>
-        </div>
+    <div class="container px-[60px] max-lg:px-8 max-sm:px-4">
 
-        @pushOnce('scripts')
+        {!! view_render_event('bagisto.shop.checkout.onepage.breadcrumbs.before') !!}
+
+        <!-- Breadcrumbs -->
+        <x-shop::breadcrumbs name="checkout"></x-shop::breadcrumbs>
+
+        {!! view_render_event('bagisto.shop.checkout.onepage.breadcrumbs.after') !!}
+
+        <v-checkout>
+            <!-- Shimmer Effect -->
+            <x-shop::shimmer.checkout.onepage/>
+        </v-checkout>
+    </div>
+
+    @pushOnce('scripts')
         <script type="text/x-template" id="v-checkout-template">
-            <div class="grid grid-cols-[1fr_auto] gap-[30px] max-lg:grid-cols-[1fr]">
+            <div class="grid grid-cols-[1fr_auto] gap-8 max-lg:grid-cols-[1fr]">
                 <div    
                     class="overflow-y-auto"
                     ref="scrollBottom"
                 >
+                    {!! view_render_event('bagisto.shop.checkout.onepage.addresses.before') !!}
+
                     @include('shop::checkout.onepage.addresses.index')
+
+                    {!! view_render_event('bagisto.shop.checkout.onepage.addresses.after') !!}
+
+                    {!! view_render_event('bagisto.shop.checkout.onepage.shipping_method.before') !!}
 
                     @include('shop::checkout.onepage.shipping')
 
+                    {!! view_render_event('bagisto.shop.checkout.onepage.shipping_method.after') !!}
+
+                    {!! view_render_event('bagisto.shop.checkout.onepage.payment_method.before') !!}
+
                     @include('shop::checkout.onepage.payment')
 
+                    {!! view_render_event('bagisto.shop.checkout.onepage.payment_method.before') !!}
                 </div>
                 
                 @include('shop::checkout.onepage.summary')
@@ -58,23 +91,20 @@
                 data() {
                     return {
                         cart: {},
-                        idKota: '',
+
                         isCartLoading: true,
-                        shippingMethods: [],
                     }
                 },
 
                 created() {
                     this.getOrderSummary();
-                },
+                }, 
 
                 methods: {
                     getOrderSummary() {
                         this.$axios.get("{{ route('shop.checkout.onepage.summary') }}")
                             .then(response => {
                                 this.cart = response.data.data;
-                                console.log(this.cart)
-                                this.idKota = this.cart.billing_address.id_kota; // Simpan idKota dalam variabel data
 
                                 this.isCartLoading = false;
 
@@ -89,22 +119,8 @@
                             })
                             .catch(error => console.log(error));
                     },
-                    getCost() {
-                        this.$axios.get("{{ route('shop.api.core.cost') }}", {
-                                params: {
-                                    id_city: this.idKota // Gunakan this.idKota yang sudah disimpan sebelumnya
-                                }
-                            })
-                            .then(response => {
-                                this.cost = response.data.cost[0]['costs'];
-                                this.shippingMethods = response.data.shippingMethods; // Pastikan ini sesuai dengan struktur yang diharapkan
-                                console.log(this.cost)
-                                // Lakukan sesuatu dengan this.cost
-                            })
-                            .catch(error => console.error(error));
-                    },
                 },
             });
         </script>
-        @endPushOnce
+    @endPushOnce
 </x-shop::layouts>
